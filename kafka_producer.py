fe581e36aae7217ab9c6b879256d0eb76d3e070a
rohit-anilkumar <rohit.anilkumar@fullcontact.com>
2020-12-06 18:47:20 +0530

    Modify topic publishing strategy

--- kafka_producer.py
+++ kafka_producer.py
@@ -10,32 +10,55 @@ from kafka import KafkaProducer
 import time
 import json
 
+
 def json_serializer(payload):
+    """
+    
+
+    Parameters
+    ----------
+    payload : Dict
+        Dictionary of data values that needs to be serialized before sending to Kafka topics.
+
+    Returns
+    -------
+    return_payload : json
+        json data encoded to utf-8
+        
+    """
     return_payload = json.dumps(payload).encode('utf-8')
     return return_payload
 
- 
+
 if __name__=='__main__':
     bbc = BBCParser()
     prev_top_news = None
+    top_news = None
     
     bootstrap_servers = '127.0.0.1:9092'
     client_id = 'bbc_feed_publisher'
+    topic = 'bbcfeed'
+    retries = 5
     
-    producer = KafkaProducer(bootstrap_servers = bootstrap_servers, client_id = client_id)
+    producer = KafkaProducer(bootstrap_servers = bootstrap_servers, client_id = client_id, retries = retries)
     
     while(True):
         status_code, items = bbc.getResponse()
         if(status_code == 200):
-            parser_output, top_news = bbc.responseParser(items)
-        
+            parser_output = bbc.responseParser(items)
+            final_news, top_news = bbc.newsOrganiser(parser_output)
+            
         #Condition to check if its necessary to publish message to Kafka or not
         if(top_news == prev_top_news):
             print("Do not publish to Kafka")
         else:
-            for news in parser_output:
+            for news in final_news:
                 if(news!= prev_top_news):        
                     print("Publishing to Kafka")
-                    producer.send('bbcfeed', value=json_serializer(news))
+                    producer.send(topic, value=json_serializer(news))
+                    time.sleep(0.5)
+                else:
+                    break
             prev_top_news = top_news
-        time.sleep(3)
+        print('Producer Disabled for 1 hr')
+        time.sleep(3600)
